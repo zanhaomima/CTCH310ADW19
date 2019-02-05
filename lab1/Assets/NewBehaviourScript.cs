3a2f79a43deb942b4c140d73aa2fd042bf035a7e
jf.z <jzm681@udml13.cs.uregina.ca>
2019-02-05 11:13:59 -0600

    lab4

--- lab1/Assets/NewBehaviourScript.cs
+++ lab1/Assets/NewBehaviourScript.cs
@@ -4,38 +4,60 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private Camera c;
     // Start is called before the first frame update
     void Start()
     {
         print("hello");
+        //c = transform.localRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        move();
+    }
+
+    void move() {
+
+        Cursor.lockState = CursorLockMode.Locked;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             Vector3 position = this.transform.position;
             position.z++;
             this.transform.position = position;
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow)) 
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             Vector3 position = this.transform.position;
             position.z--;
             this.transform.position = position;
         }
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             Vector3 position = this.transform.position;
             position.x++;
             this.transform.position = position;
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Vector3 position = this.transform.position;
             position.x--;
             this.transform.position = position;
+        }
+
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+        //string s ="x"+this.transform.rotation.x+" y"+ this.transform.rotation.y+" z"+this.transform.rotation.z;
+        //print(s);
+        transform.Rotate(-v, h, 0);
+        //this.transform.localRotation = transform.localRotation * Quaternion.Euler(-v, 0, 0);
+        //this.transform.localRotation = transform.localRotation * Quaternion.Euler(0, h, 0);
+        if (this.transform.rotation.eulerAngles.z != 0) {
+            this.transform.Rotate(0, 0, -this.transform.rotation.eulerAngles.z);
         }
     }
 }
