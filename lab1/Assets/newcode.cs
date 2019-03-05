b6eeba7583c48071b6f8b81696d496391b306664
jf.z <jzm681@udml13.cs.uregina.ca>
2019-03-05 10:39:49 -0600

    lab6

--- lab1/Assets/newcode.cs
+++ lab1/Assets/newcode.cs
@@ -4,6 +4,40 @@ using UnityEngine;
 
 public class newcode : MonoBehaviour
 {
+    public int intel = 5;
+    float test() 
+    {
+        return 5.0f; 
+    }
+
+    void Greet()
+    {
+        switch (intel) {
+            case 5:
+                print("Hello, good sir! Do you l,ike physics?");
+                break;
+
+            case 4:
+                print("xxx");
+                break;
+
+            case 3:
+                print("xxx");
+                break;
+
+            case 2:
+                print("xxx");
+                break;
+
+            case 1:
+                print("xxx");
+                break;
+
+            default:
+                print("??");
+                break;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
